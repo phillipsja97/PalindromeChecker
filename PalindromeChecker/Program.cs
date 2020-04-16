cf6d6f34c2d3eeb82b73c71cf3180503c1256abd
Jamie <phillipsja97@gmail.com>
2020-04-16 13:25:27 -0500

    Add project files.

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
